8ad43ea714776ee97746c6c6f644efef7376e77a
st234pa <syoon1@stuy.edu>
2016-10-19 09:56:01 -0400

    fixed some typos but still doesn't work

--- getAvg.py
+++ getAvg.py
@@ -10,14 +10,14 @@ d = {}
 
 for r in t:
         name = r[0]
-        dict[n] = [0,0,0]
+        dict[name] = [0,0,0]
 
 for r in t:
         name = r[0]
         sid = r[1]
         grade = [2]
         d[name][0] += grade
-        d[name][1] sid
+        d[name][1] = sid
         d[name][2] += 1
 
 for key in d:
